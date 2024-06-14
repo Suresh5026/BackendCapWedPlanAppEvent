7789e69abf137b0208a5b156531faa1c08cea5fc
Suresh5026 <suresh361994@gmail.com>
2024-06-14 12:17:38 +0530

    lacommit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const HOSTNAME = process.env.HOSTNAME || 'localhost';
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://statuesque-granita-6f354c.netlify.app', 
+    origin: 'https://steady-heliotrope-43be37.netlify.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
